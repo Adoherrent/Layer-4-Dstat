4bc604472aecb42b0c946937d91197b95931e25e
Adoherrent <96074280+Adoherrent@users.noreply.github.com>
2022-03-12 19:01:44 -0500

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,41 @@
+
+<?php
+/*
+  _____ ____      _    ____ _____ _   _ ____  _____ 
+ |  ___|  _ \    / \  / ___|_   _| | | |  _ \| ____|
+ | |_  | |_) |  / _ \| |     | | | | | | |_) |  _|  
+ |  _| |  _ <  / ___ \ |___  | | | |_| |  _ <| |___ 
+ |_|   |_| \_\/_/   \_\____| |_|  \___/|_| \_\_____|
+
+*/
+
+//EN - FR
+//CONFIGURATION - CONFIG
+
+
+//SITE NAME - Nom du site
+$sitename = 'ANACHAKHACKER.COM';
+
+
+// CAPS ONLY - Majuscule seulement
+
+
+//Bit or byte - Bit ou octet
+// US=bit and/et EU= octet
+$num = 'US';
+
+//Data transferred or received  -  Donnée transférée ou recus
+//RX = received/recus TX = transferred/transféerée
+$datatype = 'RX';
+
+//Language - Langue
+//FR = francais/french - EN = english/anglais
+$lang = 'EN';
+
+//IP OF SERVER - Ip du serveur
+$ip = $_SERVER['46.166.185.62'];
+
+//Name of interface - Nom de l'interface
+//Usually : 'eth0' - Generalement :'eth0'
+$interface = 'eth0';
+?>
